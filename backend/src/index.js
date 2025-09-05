9eaf5a6f9b0dbd80b0af4a962b1f8ba581d04630
khoa0402 <khoadtnl@gmail.com>
2025-09-05 17:16:48 +0700

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -64,8 +64,8 @@ app.use("/api/status", statusRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
